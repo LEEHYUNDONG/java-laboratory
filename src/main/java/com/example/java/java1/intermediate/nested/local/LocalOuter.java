a03805b4c4f7f5ffa90fad3f93cebcdfbd123b47
easttwave <hd.lee6@cj.net>
2024-08-19 17:10:14 +0900

    이현동 #comment java - 내부, 중첩 클래스

---
+++ src/main/java/com/example/java/java1/intermediate/nested/local/LocalOuter.java
@@ -0,0 +1,39 @@
+package com.example.java.java1.intermediate.nested.local;
+
+/**
+ * @author : lhd
+ * @packageName : com.example.java.java1.intermediate.nested.local
+ * @fileName : java
+ * @description :
+ * <p>
+ * ====================================================
+ * DATE            AUTHOR              NOTE
+ * ====================================================
+ * 2024-08-19         lhd
+ */
+public class LocalOuter {
+    private int outInstanceVar = 2;
+
+    public void process(int paramVar) {
+        int localVar = 412;
+
+        class LocalPrinterClass {
+            int val = 32;
+
+            public void printData() {
+                System.out.println("val = " + val);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+
+        LocalPrinterClass localPrinterClass = new LocalPrinterClass();
+        localPrinterClass.printData();
+
+    }
+    public static void main(String[] args) {
+        LocalOuter localOuter = new LocalOuter();
+        localOuter.process(10);
+    }
+}
---
+++ src/main/java/com/example/java/java1/intermediate/object1/Car.java
@@ -0,0 +1,18 @@
+package com.example.java.java1.intermediate.object1;
+
+/**
+ * @author : lhd
+ * @packageName : com.example.java.java1.intermediate.object1
+ * @fileName : java
+ * @description :
+ * <p>
+ * ====================================================
+ * DATE            AUTHOR              NOTE
+ * ====================================================
+ * 2024-08-19         lhd
+ */
+public class Car {
+    public void go() {
+        System.out.println("자동차가 갑니다.");
+    }
+}
