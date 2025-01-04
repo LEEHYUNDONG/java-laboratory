a98ec5d1cfaaaf3f6d504dfd776ccc5206dda8b4
LEEHYUNDONG <l.steve10@yahoo.com>
2025-01-04 16:15:50 +0900

    test : Jtest Test 코드 작성

--- src/test/java/com/example/java/work/practice/junit/junit/entity/JTestTest.java
+++ src/test/java/com/example/java/work/practice/junit/junit/entity/JTestTest.java
@@ -1,20 +1,29 @@
 package com.example.java.work.practice.junit.junit.entity;
 
 import com.example.java.work.practice.junit.calculator.entity.CalculatorTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
 
 
 class JTestTest {
 
     @Test
-    void runTest() {
+    void runTest() throws InvocationTargetException, IllegalAccessException, ClassNotFoundException, InstantiationException {
         //given
+        int passed = 4;
+        int failed = 0;
         JTest jTest = new JTest();
+        String resultMessageExpected = "passed: " + passed + " failed: " + failed;
+
         //when
-        jTest.run(CalculatorTest.class);
+        JTestResult result = jTest.run(CalculatorTest.class);
+
 
         //then
-
-
+        // 테스트 결과와 테스트가 성공을 출력하는지 확인
+        Assertions.assertThat(result.getTestStatus()).isEqualTo(TestStatus.SUCCESS);
+        Assertions.assertThat(result.printMessage()).isEqualTo(resultMessageExpected);
     }
 }
