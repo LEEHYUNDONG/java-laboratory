58defc5118c72f37d0295c467e37f70a7848d3d3
LEEHYUNDONG <l.steve10@yahoo.com>
2025-06-10 16:47:33 +0900

    java exception

--- src/main/java/com/example/java/lang/intermediate/exception/ex4/exception/NetworkClientExceptionV4.java
+++ src/main/java/com/example/java/lang/intermediate/exception/ex4/exception/NetworkClientExceptionV4.java
@@ -1,6 +1,6 @@
 package com.example.java.lang.intermediate.exception.ex4.exception;
 
-public class NetworkClientExceptionV4 extends Exception {
+public class NetworkClientExceptionV4 extends RuntimeException {
     private final String message;
 
     public NetworkClientExceptionV4(String message) {
